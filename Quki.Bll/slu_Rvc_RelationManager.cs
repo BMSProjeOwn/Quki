9cea289375a08454877f438fb278d0d9675a6f0d
X <X@MCICEK-LTP>
2023-01-09 12:28:50 +0300

    ...

--- Quki.Bll/slu_Rvc_RelationManager.cs
+++ Quki.Bll/slu_Rvc_RelationManager.cs
@@ -74,7 +74,7 @@ namespace Quki.Bll
                               SwR = SwR
                           })
 
-                          .Where(w => w.R.P.rvc_seq == 10 && w.R.D.slu_type == "MI").OrderBy(x => x.R.D.control_number.Value)
+                          .Where(w => w.R.P.rvc_seq == 2 && w.R.D.slu_type == "MI").OrderBy(x => x.R.D.control_number.Value)
                           .Select(s => new SluDefModel
                           {
                               slu_def_name = s.R.D.slu_def_name,
