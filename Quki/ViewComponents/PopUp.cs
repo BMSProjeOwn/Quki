8d09fca71edee44ff7f0f123c0171a7a12c81fa8
X <X@MCICEK-LTP>
2023-03-15 17:59:48 +0300

    ...

---
+++ Quki/ViewComponents/PopUp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Quki.Common;
+using Quki.Dal.Concrete.Entityframework.Context;
+using Quki.Interface;
+using Quki.Dal.Abstract;
+using Quki.Entity.DtoModels;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Quki.ViewComponents
+{
+    public class PopUp : ViewComponent
+    {
+        private readonly INewsAndAnnouncementService service;
+
+
+        public PopUp(INewsAndAnnouncementService service)
+        {
+            this.service = service;
+
+        }
+        public IViewComponentResult Invoke()
+        {
+            var league = Common.Functions.setLanguage(Request.Cookies[".AspNetCore.Culture"]);
+            var model = service.GetNewsAndAnnouncementListWithLangueage(league).OrderBy(x=>x.NewsAndAnnouncementSeqId).Take(1).FirstOrDefault();
+
+
+            return View(model);
+        }
+    }
+}
