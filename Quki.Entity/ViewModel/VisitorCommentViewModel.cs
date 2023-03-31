de86b61595d6d05eb657f62add1f2874b62fd84e
X <X@MCICEK-LTP>
2023-03-31 13:12:08 +0300

    ...

---
+++ Quki.Entity/ViewModel/VisitorCommentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Quki.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quki.Entity.ViewModel
+{
+    public class VisitorCommentViewModel
+    {
+       public List<VisitorComment> VisitorComments { get; set; }
+        public VisitorComment VisitorComment { get; set; }
+
+    }
+}
